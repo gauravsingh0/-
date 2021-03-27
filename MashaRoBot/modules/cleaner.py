debc3ebd27e48fb9bcd3d4844c88b0cf3ddab2c0
☞丂ﾑﾶﾶƳ☜ <76897487+MrSammyXD@users.noreply.github.com>
2021-03-27 18:53:08 +0530

    Update cleaner.py

--- MashaRoBot/modules/cleaner.py
+++ MashaRoBot/modules/cleaner.py
@@ -220,13 +220,13 @@ def bluetext_ignore_list(update: Update, context: CallbackContext):
     return
 
 
-__help__ = """
-*Blue text cleaner* removed any made up commands that people send in your chat.
- ❍ /cleanblue <on/off/yes/no>*:* clean commands after sending
- ❍ /ignoreblue <word>*:* prevent auto cleaning of the command
- ❍ /unignoreblue <word>*:* remove prevent auto cleaning of the command
- ❍ /listblue*:* list currently whitelisted commands
-"""
+# __help__ = """
+# *Blue text cleaner* removed any made up commands that people send in your chat.
+# ❍ /cleanblue <on/off/yes/no>*:* clean commands after sending
+# ❍ /ignoreblue <word>*:* prevent auto cleaning of the command
+# ❍ /unignoreblue <word>*:* remove prevent auto cleaning of the command
+# ❍ /listblue*:* list currently whitelisted commands
+# """
 
 SET_CLEAN_BLUE_TEXT_HANDLER = CommandHandler("cleanblue", set_blue_text_must_click)
 ADD_CLEAN_BLUE_TEXT_HANDLER = CommandHandler("ignoreblue", add_bluetext_ignore)
@@ -250,7 +250,7 @@ dispatcher.add_handler(REMOVE_CLEAN_BLUE_TEXT_GLOBAL_HANDLER)
 dispatcher.add_handler(LIST_CLEAN_BLUE_TEXT_HANDLER)
 dispatcher.add_handler(CLEAN_BLUE_TEXT_HANDLER, BLUE_TEXT_CLEAN_GROUP)
 
-__mod_name__ = "Cleaner"
+# __mod_name__ = "Cleaner"
 __handlers__ = [
     SET_CLEAN_BLUE_TEXT_HANDLER,
     ADD_CLEAN_BLUE_TEXT_HANDLER,
