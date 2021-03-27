baba2709977d36c356648a3eee122edafbf0a4ac
☞丂ﾑﾶﾶƳ☜ <76897487+MrSammyXD@users.noreply.github.com>
2021-03-27 17:25:26 +0530

    Update __nsfw_watch.py

--- MashaRoBot/modules/__nsfw_watch.py
+++ MashaRoBot/modules/__nsfw_watch.py
@@ -110,10 +110,10 @@ async def ws(event):
             return
 
 
-__help__ = """
-Masha can protect your group from NSFW senders
- ❍ /addnsfw*:* Adds The Group to nsfw Watch List
- ❍ /rmnsfw*:* Removes The Group From nsfw Watch List
-"""
+# __help__ = """
+# Masha can protect your group from NSFW senders
+# ❍ /addnsfw*:* Adds The Group to nsfw Watch List
+# ❍ /rmnsfw*:* Removes The Group From nsfw Watch List
+# """
 
-__mod_name__ = "NSFW"
+# __mod_name__ = "NSFW"
