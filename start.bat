714e984faa01662622e0f0c8492171b9eee8ae73
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-04-21 22:37:08 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Masha Robot
-:: Enables virtual env mode and then starts Masha
-env\scripts\activate.bat && py -m MashaRoBot
+TITLE Patricia
+:: Enables virtual env mode and then starts Patric
+env\scripts\activate.bat && py -m Patricia
